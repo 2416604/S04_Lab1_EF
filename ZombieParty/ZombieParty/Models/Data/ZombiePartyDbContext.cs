b886106d257490c30abde999b6f632450206c561
Jonathan Albert Randrianarivony <2416604@cegepmontpetit.ca>
2026-09-08 16:03:18 -0400

    1
    1

---
+++ ZombieParty/ZombieParty/Models/Data/ZombiePartyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZombieParty.Models.Data
+{
+    public class ZombiePartyDbContext
+    {
+    public ZombiePartyDbContext(DbContextOptions<ZombiePartyDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
